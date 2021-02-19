81edcc6397b0546f144439c263bd8437f52c1a5c
David Tabor <1davidtabor1@gmail.com>
2021-02-19 11:34:33 -0800

    Removed font function

--- main.py
+++ main.py
@@ -15,11 +15,6 @@ VEL = 10
 
 PLAYER_TOKEN_IMAGE = pygame.image.load(os.path.join('Assets', 'jake_drawing.png'))
 PLAYER_TOKEN = pygame.transform.scale(PLAYER_TOKEN_IMAGE, (CHARACTER_WIDTH, CHARACTER_HEIGHT))
-print("Initializing fonts")
-fonts = pygame.font.get_fonts()
-print(len(fonts))
-for f in fonts:
-    print(f)
 
 def draw_window(lamp):
     WIN.fill(WHITE)
