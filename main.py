8e778a241e4aa594c407480de317ae8600e03279
David Tabor <1davidtabor1@gmail.com>
2021-02-19 13:20:24 -0800

    Changed the way the background updates

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ WHITE = (255, 255, 255)
 FPS = 60
 VEL = 10
 
+BACKGROUND_IMAGE = pygame.image.load(os.path.join('Assets','background.jpg'))
+BACKGROUND_BOX = WIN.get_rect()
+
 PLAYER_TOKEN_IMAGE = pygame.image.load(os.path.join('Assets', 'jake_drawing.png'))
 PLAYER_TOKEN = pygame.transform.scale(PLAYER_TOKEN_IMAGE, (CHARACTER_WIDTH, CHARACTER_HEIGHT))
 
-def draw_window(lamp):
-    WIN.fill(WHITE)
+def draw_window(lamp, previous_location):
+    WIN.blit(BACKGROUND_IMAGE, previous_location, (previous_location[0], previous_location[1], CHARACTER_HEIGHT, CHARACTER_WIDTH))
     WIN.blit(PLAYER_TOKEN, (lamp.x, lamp.y))
     pygame.display.update()
 
@@ -35,8 +38,11 @@ def lamp_handle_movement(keys_pressed, lamp):
 def main():
     lamp = pygame.Rect(100, 300, CHARACTER_WIDTH, CHARACTER_HEIGHT)
 
+    WIN.blit(BACKGROUND_IMAGE, BACKGROUND_BOX)
+
     clock = pygame.time.Clock()
     run = True
+
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -44,9 +50,9 @@ def main():
                 run = False
                 
         keys_pressed = pygame.key.get_pressed()
+        previous_location = (lamp.x, lamp.y)
         lamp_handle_movement(keys_pressed, lamp)
-        draw_window(lamp)
-
+        draw_window(lamp, previous_location)
         
     pygame.quit()
 
